1f4c7e2df2e84bccb2b0246eae96b0cb8393a191
noldikrisandi <krisandinoldi@gmail.com>
2024-01-25 16:59:37 +0700

    komit pertama

---
+++ js/menu.js
@@ -0,0 +1,30 @@
+// JavaScript untuk memuat menu
+document.addEventListener("DOMContentLoaded", function () {
+  const menuContainer = document.getElementById("menu-container");
+  const nav = document.createElement("nav");
+  nav.innerHTML = `
+    <div class="menu" id="navbar">
+    <div class="top"></div>
+    <div class="main-nav">
+      <a href="#" id="home-button" class="active"><h1>MediaInfo</h1></a>
+    </div>
+    <div class="sub-nav">
+      <nav>
+        <ul>
+          <li>Menu 1</li>
+          <li>
+            Menu 2
+            <ul>
+              <li>Submenu 1</li>
+              <li>Submenu 2</li>
+            </ul>
+          </li>
+          <li>Menu 3</li>
+          <li>Menu 4</li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  `;
+  menuContainer.appendChild(nav);
+});
